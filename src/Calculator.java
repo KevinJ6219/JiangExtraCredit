bb3cc1bf062cbd11301720346d85fdf66bd06d6c
Kevin Jiang <kevinj66@nycstudents.net>
2022-10-29 17:26:49 -0400

    Final

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Calculator {
+public class Calculator {
+
+    private double num1;
+    private double num2;
+    private String operation;
+
+    public Calculator(double num1, double num2, String operation) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.operation = operation;
+    }
+
+    public double calculate() {
+        if (operation.equals("+")) {
+            return num1 + num2;
+        }
+        else if (operation.equals("-")) {
+            return num1 - num2;
+        }
+        else if (operation.equals("*")) {
+            return num1 * num2;
+        }
+        else if (operation.equals("/")) {
+            return num1 / num2;
+        }
+        else if (operation.equals("%")) {
+            return num1 % num2;
+        }
+        else if (operation.equals("^")) {
+            return Math.pow(num1, num2);
+        }
+        else {
+            return -1;
+        }
+    }
 }
--- src/CalculatorRunner.java
+++ src/CalculatorRunner.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class CalculatorRunner {
+import java.util.Scanner;
+
+public class CalculatorRunner {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String expression = "";
+        double num1, num2;
+        String operation;
+        System.out.print("Enter an expression with *, /, +, -, %, or ^: ");
+        expression = input.nextLine();
+
+        int length = expression.length() - 1;
+        int indexOfSpace = expression.indexOf(" ");
+        num1 = Integer.parseInt(expression.substring(0,indexOfSpace));
+        num2 = Integer.parseInt(expression.substring(indexOfSpace + 3));
+        operation = expression.substring(indexOfSpace + 1, indexOfSpace + 2);
+
+        Calculator expression1 = new Calculator(num1, num2, operation);
+        if (expression1.calculate() == -1) {
+            System.out.println("There is no such operation.");
+        }
+        else {
+            System.out.println(expression1.calculate());
+        }
+    }
 }
