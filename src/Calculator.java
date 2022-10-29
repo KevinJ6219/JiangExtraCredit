328f58f6029cf979ca2a73bcf64e56e8ceaefeec
Kevin Jiang <kevinj66@nycstudents.net>
2022-10-29 17:30:28 -0400

    Added comments

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,15 +1,16 @@
 public class Calculator {
-
+    // Instance variables
     private double num1;
     private double num2;
     private String operation;
 
+    //Constructor
     public Calculator(double num1, double num2, String operation) {
         this.num1 = num1;
         this.num2 = num2;
         this.operation = operation;
     }
-
+    //Method checks what operation the user is using and returns the calculations
     public double calculate() {
         if (operation.equals("+")) {
             return num1 + num2;
--- src/CalculatorRunner.java
+++ src/CalculatorRunner.java
@@ -4,19 +4,26 @@ public class CalculatorRunner {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+
         String expression = "";
         double num1, num2;
         String operation;
+
+        //Asking user for a expression
         System.out.print("Enter an expression with *, /, +, -, %, or ^: ");
         expression = input.nextLine();
 
+        //Parsing out the numbers and the operation
         int length = expression.length() - 1;
         int indexOfSpace = expression.indexOf(" ");
         num1 = Integer.parseInt(expression.substring(0,indexOfSpace));
         num2 = Integer.parseInt(expression.substring(indexOfSpace + 3));
         operation = expression.substring(indexOfSpace + 1, indexOfSpace + 2);
 
+        //Using the parsed out numbers and operations, put them in the parameters of an instance of the Calculator class
         Calculator expression1 = new Calculator(num1, num2, operation);
+
+        //Prints out the calculation. If the user enters a operation that is not valid the system will tell the user.
         if (expression1.calculate() == -1) {
             System.out.println("There is no such operation.");
         }
